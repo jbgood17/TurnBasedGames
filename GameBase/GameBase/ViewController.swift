1be34525381598dd88b7ebbde9e4334ae5ae4556
John C <cheneyjohnm@gmail.com>
2020-03-22 14:28:43 -0400

    Initial Commit

---
+++ GameBase/GameBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameBase
+//
+//  Created by John Cheney on 3/22/20.
+//  Copyright © 2020 John Cheney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
