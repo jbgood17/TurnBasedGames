0d5b8a02f5af0862ecce370fd7b855c062c87fda
John C <cheneyjohnm@gmail.com>
2020-03-22 14:43:37 -0400

    Adding another project for InOrOut game. Not sure if this is the right track. But it's there.

---
+++ InOrOut/InOrOut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InOrOut
+//
+//  Created by John Cheney on 3/22/20.
+//  Copyright © 2020 John Cheney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+}
+
